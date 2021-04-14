d43150b4c32ab5be4ded36a8e012e8f7455156d2
Alex <loban.sasha.97@mail.ru>
2021-04-14 17:40:03 +0300

    nundler update -one class for several nodes

--- client.py
+++ client.py
@@ -11,16 +11,22 @@ class SubHandler(object):
 
     """
     Client to subscription. It will receive events from server
+    
+    tags: list of all processed network nodes;
+    storage: multilist for recording the values of processed nodes;
+    
     """
 
-    def __init__(self):
-        self.storage = []
+    def __init__(self, tags):   
+        self.tags = tags
+        self.storage = [ [] for i in range(len(tags)) ]
 
     def datachange_notification(self, node, val, data):
-        print("Python: New data change event", node, val)
-        self.storage.append(val)
-
-
+       i = self.tags.index(node)
+       self.storage[i].append(val)
+       
+       print("Python: New data change event", node, val)
+            
     def event_notification(self, event):
         print("Python: New event", event)
 
@@ -34,29 +40,23 @@ def main():
         tag1 = client.get_node("ns=2;s=Channel1.Device1.Tag1")
         tag2 = client.get_node("ns=2;s=Simulation Examples.Functions.Ramp3")
         tag3 = client.get_node("ns=2;s=Simulation Examples.Functions.Sine1")
-
-        handler1 = SubHandler()
-        sub1 = client.create_subscription(0, handler1)
-        sub1.subscribe_data_change(tag1)
-
-        handler2 = SubHandler()
-        sub2 = client.create_subscription(0, handler2)
-        sub2.subscribe_data_change(tag2)
-
-        handler3 = SubHandler()
-        sub3 = client.create_subscription(0, handler3)
-        sub3.subscribe_data_change(tag3)
+        
+        tags = [tag1, tag2, tag3]
+  
+        handler = SubHandler(tags)
+        sub = client.create_subscription(0, handler)
+        
+        handle1 = sub.subscribe_data_change(tag1)
+        handle2 = sub.subscribe_data_change(tag2)
+        handle3 = sub.subscribe_data_change(tag3)
 
         sleep(0.1)
 
         embed()
 
-        print("___1___", handler1.storage)
-        print("___2___", handler2.storage)
-        print("___3___", handler3.storage)
-        
     finally:
         client.disconnect()
+        print(handler.storage)
 
 
 if __name__ == "__main__":
